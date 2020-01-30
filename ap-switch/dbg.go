9cdd09e8d6516b7818b77f09ea0434096517da92
Pawel Foremski <pjf@iitis.pl>
2020-01-30 17:08:42 +0100

    ap-switch 0.1

--- ap-switch/dbg.go
+++ ap-switch/dbg.go
@@ -1,13 +1,13 @@
 /*
- * ap-switch - Autopolicy PoC
- * Copyright (C) 2018-2019 IITiS PAN Gliwice <https://www.iitis.pl/>
+ * Autopolicy PoC
+ * Copyright (C) 2019-2020 IITiS PAN Gliwice <https://www.iitis.pl/>
  * Author: Pawel Foremski <pjf@iitis.pl>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
